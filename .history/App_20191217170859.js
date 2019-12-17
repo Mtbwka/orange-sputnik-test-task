4b7dbfa5591c0853bae0c71025befe8a60f2be65
Viktor <meow.lululu@gmail.com>
2019-12-17 17:12:07 +0200

    initial commit

---
+++ .history/App_20191217170859.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log('123');
+  return (
+    <View style={styles.container}>
+      <Text>Hellooooo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
